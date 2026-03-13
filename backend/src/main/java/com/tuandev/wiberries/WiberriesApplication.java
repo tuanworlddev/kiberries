f0f0ff4f981d0140c47107a1889bc42086059544
tuanworlddev <nanhtuan.it@gmail.com>
2026-03-14 00:06:37 +0700

    Init project e-commerce

---
+++ backend/src/main/java/com/tuandev/wiberries/WiberriesApplication.java
@@ -0,0 +1,13 @@
+package com.tuandev.wiberries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiberriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiberriesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tuandev/wiberries/WiberriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuandev.wiberries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiberriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
